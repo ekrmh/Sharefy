d8bf53f55f126de4381d25c4b0703419ffa9464f
i00598949 <ismail.emre.bayirli1@huawei.com>
2021-11-27 00:15:44 +0300

    bottom nav added map added

--- app/src/main/java/com/sharefy/android/ui/activity/main/MainActivity.kt
+++ app/src/main/java/com/sharefy/android/ui/activity/main/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.sharefy.android.ui.activity.main
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import com.sharefy.android.R
 import com.sharefy.android.base.BaseActivity
 import com.sharefy.android.databinding.ActivityMainBinding
@@ -16,6 +20,29 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
 
     override val fragmentContainerId = R.id.navHostFragmentContainer
 
-    override fun onReady(savedInstanceState: Bundle?) {
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                // Precise location access granted.
+            }
+            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                // Only approximate location access granted.
+            }
+            else -> {
+                // No location access granted.
+            }
+        }
     }
+
+    override fun onReady(savedInstanceState: Bundle?) {
+        locationPermissionRequest.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION))
+    }
+
+
 }
---
+++ app/src/main/java/com/sharefy/android/ui/fragment/map/MapFragment.kt
@@ -0,0 +1,64 @@
+package com.sharefy.android.ui.fragment.map
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.viewModels
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.sharefy.android.R
+import com.sharefy.android.base.BaseFragment
+import com.sharefy.android.databinding.FragmentMapBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class MapFragment : BaseFragment<FragmentMapBinding, MapViewModel>() {
+
+    override val layoutId: Int = R.layout.fragment_map
+
+    override val viewModel: MapViewModel by viewModels()
+
+    private val fusedLocationClient: FusedLocationProviderClient by lazy {
+        LocationServices.getFusedLocationProviderClient(requireActivity())
+    }
+
+    override fun onReady(savedInstanceState: Bundle?) {
+
+
+        initMap()
+    }
+
+    private fun initMap() {
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync {
+
+        }
+    }
+
+    private fun getLastKnownLocation() {
+        if (ActivityCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener {
+
+            }
+    }
+
+}
