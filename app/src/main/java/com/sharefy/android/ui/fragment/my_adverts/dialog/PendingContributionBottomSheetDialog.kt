2610fcd08cbfe101f9464ad7e270f41bca89a324
ekrmh <ekrem.hatipoglu@outlook.com>
2021-11-27 10:20:46 +0300

    profile added and localization

--- app/src/main/java/com/sharefy/android/ui/fragment/my_adverts/dialog/PendingContributionBottomSheetDialog.kt
+++ app/src/main/java/com/sharefy/android/ui/fragment/my_adverts/dialog/PendingContributionBottomSheetDialog.kt
@@ -24,24 +24,36 @@ class PendingContributionBottomSheetDialog(
     override val viewModel: MyAdvertsViewModel by viewModels()
 
     private val adapter = PendingContAdapter(listOf(), this)
+    private val allList = mutableListOf<ContributeAdvert>()
 
     override fun init(savedInstanceState: Bundle?) {
         binding.recyclerViewPending.adapter = adapter
 
-        adapter.updateData(advert.necessaryMaterial[ind].pendingContribution)
+        advert.necessaryMaterial.map { it.pendingContribution }.forEach {
+            allList.addAll(it)
+        }
+
+        adapter.updateData(allList)
     }
 
     override fun onApproveContributionClicked(item: ContributeAdvert, position: Int) {
-        advert.necessaryMaterial[ind].pendingContribution.removeAt(position)
-        advert.necessaryMaterial[ind].approvedContribution.add(item)
+        advert.necessaryMaterial.forEachIndexed { x, necessaryMaterials ->
+            necessaryMaterials.pendingContribution.forEachIndexed { y, contributeAdvert ->
+                if (contributeAdvert.count == item.count && contributeAdvert.userId == item.userId && contributeAdvert.id == item.id){
+                    advert.necessaryMaterial[x].pendingContribution.removeAt(y)
+                    advert.necessaryMaterial[x].approvedContribution.add(contributeAdvert)
 
-        advert.necessaryMaterial[ind].updatable = true
+                    advert.necessaryMaterial[x].updatable = true
 
-        viewModel.updateAdvertData(
-            advert.docId, mapOf(
-                Pair("necessaryMaterial", advert.necessaryMaterial)
-            )
-        )
-        adapter.updateData(advert.necessaryMaterial[ind].pendingContribution)
+                    viewModel.updateAdvertData(
+                        advert.docId, mapOf(
+                            Pair("necessaryMaterial", advert.necessaryMaterial)
+                        )
+                    )
+                    adapter.updateData(allList)
+                }
+            }
+        }
+
     }
 }
